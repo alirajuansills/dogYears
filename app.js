403104281f4737949d338013c03bb391d3e2b7b4
Ali Sills <alisill@mail.regent.edu>
2021-09-06 15:35:38 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// My Age
+let myAge = 27;
+// Early years will change
+let earlyYears = 2;
+// Math for early dog years
+earlyYears = earlyYears * 10.5;
+// Math for later dog years
+laterYears = myAge - 2;
