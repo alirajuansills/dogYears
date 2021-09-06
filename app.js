dffbcd81ef52ac9666cef221852269f999a51b41
Ali Sills <alisill@mail.regent.edu>
2021-09-06 16:24:45 -0400

    finished mini project

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ let earlyYears = 2;
 earlyYears = earlyYears * 10.5;
 // Math for later dog years
 laterYears = myAge - 2;
+// Dog years accounted for by my later years
+laterYears *= 4;
+// Early years and later years combined
+myAgeInDogYears = earlyYears + laterYears;
+// Making my name lower case
+const myName = "Ali".toLowerCase()
+// Logging an interpolation
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
